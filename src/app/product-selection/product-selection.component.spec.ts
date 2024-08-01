984e52a264347e0d83ea25dd6139f2e659ecfb0d
Deepak kumar <deepak19122002@gmail.com>
2024-08-01 18:32:02 +0530

    first changes

---
+++ src/app/product-selection/product-selection.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductSelectionComponent } from './product-selection.component';
+
+describe('ProductSelectionComponent', () => {
+  let component: ProductSelectionComponent;
+  let fixture: ComponentFixture<ProductSelectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductSelectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductSelectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/product-selection/product-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-selection',
+  templateUrl: './product-selection.component.html',
+  styleUrls: ['./product-selection.component.css']
+})
+export class ProductSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
